12b9550f714aaf9c22da3d1e0e98b73541e40607
Douglas M. Dwyer <dougmdwyer@hotmail.com>
2024-08-17 12:20:47 -0400

    Bump dependencies

--- build.rs
+++ build.rs
@@ -1,9 +1,12 @@
 /// Marks the crate as being compiled with unstable features enabled.
 #[rustversion::nightly]
 fn main() {
+    println!("cargo::rustc-check-cfg=cfg(unstable)");
     println!("cargo:rustc-cfg=unstable");
 }
 
 /// Marks the crate as being compiled without unstable features.
 #[rustversion::not(nightly)]
-fn main() {}
+fn main() {
+    println!("cargo::rustc-check-cfg=cfg(unstable)");
+}
